9e70ea7bd92b8243066cc29101e925a695edd156
willy <wilg1569@gmail.com>
2018-07-19 20:18:14 +0200

    Fichier app

---
+++ app.rb
@@ -0,0 +1,13 @@
+# projet mairies - on lie tous les fichiers au fichier app
+require 'bundler'
+require 'pp'
+Bundler.require
+
+$:.unshift File.expand_path("./../lib/app", __FILE__) # On ajoute directement au PATH les fichiers présents au dossier app
+require 'townhalls_scrapper'
+#require 'townhalls_mailer'
+#require 'townhalls_adder_to_db'
+#require 'townhalls_follower'
+
+Scrapper.new.store_json
+#Mailer.new.app_mail
